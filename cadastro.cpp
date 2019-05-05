742a341301ee05b5124301cfabd86c3b6cf3b170
mafth <m043.mf@gmail.com>
2019-05-05 19:51:37 -0300

    Add files via upload
    Cadastro corrigido(nada a ver aquele == true)

--- cadastro.cpp
+++ cadastro.cpp
@@ -1,6 +1,6 @@
 #include "cadastro.h"
 #include "ui_cadastro.h"
-#include<cliente.h>
+#include <cliente.h>
 #include <ldde_quartos.h>
 #include <QMessageBox>
 #include <tela_inicial.h>
@@ -19,7 +19,7 @@ cadastro::~cadastro()
 
 void cadastro::on_SalvarButton_clicked()
 {
-    //close();
+    close();
     QString id= ui->idText->text();
     QString nome= ui->nomeText->text();
     QString cpf= ui->cpfText->text();
@@ -27,18 +27,15 @@ void cadastro::on_SalvarButton_clicked()
     QString checkin= ui->checkinText->text();
     QString quarto= ui->quartoText->text();
 
+    cliente* c = new cliente(this,id ,nome,cpf,tel,checkin,quarto);
+    c->exec();
 
     LDDE l;
     for (int i=0;i<10;i++) { //-> aqui vamos ocupar os quartos
-        if(l.busca(quarto.toInt())->valor == true){
-            QMessageBox::information(this, tr("Erro"),tr("Quarto ocupado"));
-        }else {
             l.insere(quarto.toInt());
         }
-    }
 
-    cliente* c = new cliente(this,id ,nome,cpf,tel,checkin,quarto);
-    c->exec();
+
 
     id = "";
     nome = "";
